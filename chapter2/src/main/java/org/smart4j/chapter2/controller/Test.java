dd706abcf74ceb32e2f9d110738d9e1f164f1045
ikutarian <saitou0217@outlook.com>
2018-10-09 17:14:29 +0800

    logback配置

---
+++ chapter2/src/main/java/org/smart4j/chapter2/controller/Test.java
@@ -0,0 +1,13 @@
+package org.smart4j.chapter2.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Test {
+
+    private static final Logger log = LoggerFactory.getLogger(Test.class);
+
+    public static void main(String[] args) {
+        log.trace("Hello World!");
+    }
+}
